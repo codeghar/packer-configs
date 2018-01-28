5e4d7626f2aaf0617547a5efd19895636ade7173
Hamza Sheikh <code@codeghar.com>
2018-01-27 19:34:05 -0800

    Fixes & improvements

---
+++ postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+uname_r=`uname -r`
+
+export PKG_PATH="${MIRROR}/pub/OpenBSD/$uname_r/packages/$(arch -s)/"
+
+# set pkg path for users
+echo "export PKG_PATH=\"${PKG_PATH}\"" >> /root/.profile
+echo "export PKG_PATH=\"${PKG_PATH}\"" >> /home/USERNAME_CHANGEME/.profile
