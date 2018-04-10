412c7b0a8be53a69f2248fd1003bdb4bed1f8c25
Hamza Sheikh <code@codeghar.com>
2018-04-09 21:48:57 -0700

    Fix shellcheck issues

--- postinstall.sh
+++ postinstall.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-uname_r=`uname -r`
+uname_r=$(uname -r)
 
-export PKG_PATH="${MIRROR}/pub/OpenBSD/$uname_r/packages/$(arch -s)/"
+export PKG_PATH
+PKG_PATH="${MIRROR}/pub/OpenBSD/$uname_r/packages/$(arch -s)/"
 
 # set pkg path for users
-echo "export PKG_PATH=\"${PKG_PATH}\"" >> /root/.profile
-echo "export PKG_PATH=\"${PKG_PATH}\"" >> /home/USERNAME_CHANGEME/.profile
+echo "export PKG_PATH" >> /root/.profile
+echo "PKG_PATH=\"${PKG_PATH}\"" >> /root/.profile
+echo "export PKG_PATH" >> /home/USERNAME_CHANGEME/.profile
+echo "PKG_PATH=\"${PKG_PATH}\"" >> /home/USERNAME_CHANGEME/.profile
